cfdd4d72869043712712ab89639bbecd4f57921e
SvrnZml2 <flochicouf@gmail.com>
2018-10-09 15:50:27 +0200

    Initial commit

---
+++ www/resources/views/layouts/footer.blade.php
@@ -0,0 +1,90 @@
+<footer>
+<!-- footer -->
+<div class="footer">
+  <div class="container">
+    <div id="footer" class="row ">
+      <!-- footer-about -->
+      <div class="col-sm">
+          <div class="footer-widget ">
+            <div class="footer-title ">A propos</div>
+            <ul class="list-unstyled">
+              <li>
+                <a href="{{ route('laplace') }}" class="text-color-footer">La Place</a>
+              </li>
+              <li>
+                <a href="{{ route('lepole') }}" class="text-color-footer">Le Pôle E2S</a>
+              </li>
+              <li>
+                <a href="{{ route('partenaires') }}" class="text-color-footer">Nos partenaires</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+     
+         <!-- /.footer-about -->
+      <div class="col-sm-4">
+        <div class="footer-widget ">
+          <div class="footer-title">Besoin d'aide ?</div>
+          <ul class="list-unstyled">
+            <li>
+              <a href="{{ route('faq') }}" class="text-color-footer">FAQ</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+     
+      <!-- footer-links -->
+      <div class="col-sm-4">
+          <div class="footer-widget ">
+            <div class="footer-title">Infos légales</div>
+            <ul class="list-unstyled">
+              <li>
+                <a href="{{ route('mentionlegale') }}" class="text-color-footer">Mentions légales</a>
+              </li>
+              <li>
+                <a href="{{ route('cgu') }}" class="text-color-footer">Conditions générales d'utilisation</a>
+              </li>
+              <li>
+                <a href="{{ route('cgv') }}" class="text-color-footer">Conditions générales de vente</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      <!-- footer-links -->
+      <div class="col-12 align-self-center">
+        <div class="footer-widget ">
+          <ul class="list-inline">
+            <li class="list-inline-item"><a class="social-icon text-center" target="_blank" href="https://twitter.com/LaPlaceE2S"><i class="fab fa-twitter"></i></a>
+            </li>
+            <li class="list-inline-item"><a class="social-icon text-center" target="_blank" href="https://www.facebook.com/laplace.e2s/"><i class="fab fa-facebook-f"></i></a>
+            </li>
+            <li class="list-inline-item"><a class="social-icon text-center" target="_blank" href="https://www.linkedin.com/company/la-place-e2s/"><i class="fab fa-linkedin-in"></i></a>
+          </ul>
+           </div>
+           
+      <!-- meteo -->
+      <!-- <div class="container">
+        <label for="location">Entrer votre ville</label>
+        <input id="location" type="text">
+        <button class="btn-success" id="weatherLocation">Obtenez l'humidité actuelle!</button>
+          <div class="errors"></div>
+          <div class="showHumidity"></div>
+          <div class="showTemp"></div>
+      </div> -->
+       
+      
+      <!-- /.footer-links -->
+      <!-- tiny-footer -->
+    </div>
+    <hr class="footer-line">
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center ">
+        <div class="tiny-footer">
+          <p>© 2018 | Association E2S Pays de Vannes</p>
+        </div>
+      </div>
+      <!-- /. tiny-footer -->
+    </div>
+  </div>
+</div>
+</footer>
